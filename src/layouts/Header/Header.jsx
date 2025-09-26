400a5f8d269df47fb5501c7076f2babd1bc61a1e
devpull <devpull@yandex.ru>
2025-09-26 16:26:00 +0300

    Header dummy and styles

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,11 @@
+import './Header.scss';
+
 const Header = () => {
-	return <p>HEADER</p>;
+	return (
+    <header className="header">
+      <div className="header__inner container"></div>
+    </header>
+  )
 };
 
 export default Header;
