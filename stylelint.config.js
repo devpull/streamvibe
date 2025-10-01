6deaa3ca675583adb72062c5ec3bcd20f13d0602
devpull <devpull@yandex.ru>
2025-10-01 10:01:49 +0300

    bem config to stylelint and new packages for bem

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,8 +4,21 @@ export default {
 		'stylelint-config-standard-scss',
 		'stylelint-prettier/recommended',
 	],
-	plugins: ['stylelint-order', 'stylelint-scss'],
+	plugins: [
+		'stylelint-order',
+		'stylelint-scss',
+		'stylelint-selector-bem-pattern',
+	],
 	rules: {
+		'selector-class-pattern': null, // for bem
 		// 'scss/dollar-variable-pattern': '^[a-z][a-zA-Z0-9]+$',
+		'plugin/selector-bem-pattern': {
+			preset: 'bem',
+			// componentName: '[a-z-]+',
+			// componentSelectors: {
+			// 	initial: '^\\.{componentName}(?:__[a-z0-9-]+)?(?:--[a-z0-9-]+)?$',
+			// 	combined: '^\\.{componentName}(?:__[a-z0-9-]+)?(?:--[a-z0-9-]+)?$',
+			// },
+		},
 	},
 };
